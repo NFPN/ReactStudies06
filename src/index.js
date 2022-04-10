09a8300c93c3b293da8c3ed06b2c935960801688
Natanael Nunes / MadWallker <natanaelfpnunes@gmail.com>
2022-04-10 13:04:35 -0300

    edit: use custom context provider component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
+import { AuthContextProvider } from "./store/auth-context";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+        <AuthContextProvider>
+        <App/>
+        </AuthContextProvider>
+);
+
